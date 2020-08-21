bb92509e23ee3fb8fbd462a86448d75cbaec4844
Moises Mariano <moisesduartem@gmail.com>
2020-08-21 15:11:14 -0300

    feat: botao de voltar

--- views/variables/index.php
+++ views/variables/index.php
@@ -13,15 +13,26 @@
     }
     #container {
         margin-top: 20%;
+        display: flex;
+        flex-direction: column;
+    }
+    #voltar {
+        position: absolute;
+        top: 10px;
+        left: 10px;
+    }
+
+    small {
+        margin-top: 30px;
     }
 </style>
 </head>
 <body>
 
     <div id="container">
-        <h2>Walter - PHP Template Engine</h2>
         <h3>Filme: $movie</h3>
         <h3>Nome: $name</h3>
+        <a id="voltar" href="/">Voltar</a>
 
         <small><a href="http://github.com/moisesduartem/walterphp">http://github.com/moisesduartem/walterphp</a> | $year - $fullName</small>
     </div>
