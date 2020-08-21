ccbc6b4b815bc55ef35df4bd769d1c88f338b654
Moises Mariano <moisesduartem@gmail.com>
2020-08-21 15:03:05 -0300

    feat: add title

--- views/variables/index.php
+++ views/variables/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Walter Template Engine</title>
 <style>
     * {
         font-family: Arial, Helvetica, sans-serif;
