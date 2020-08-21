6ce3cd0438f87f2d22b1b0521aa0eeb68705f0fc
Moises Mariano <moisesduartem@gmail.com>
2020-08-21 15:10:28 -0300

    feat: link p/ examples/variables.php

---
+++ views/home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Walter Template Engine</title>
+<style>
+    * {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    body {
+        text-align: center;
+    }
+    #container {
+        margin-top: 20%;
+        display: flex;
+        flex-direction: column;
+    }
+    #links {
+        margin: 20px;
+        padding: 20px;
+        border: 1px solid #00000088;
+    }
+
+</style>
+</head>
+<body>
+
+    <div id="container">
+        <h2>Walter - PHP Template Engine</h2>
+        <div id="links">
+
+        <a href="examples/variables.php">Variáveis</a>
+        </div>
+
+        <small><a href="http://github.com/moisesduartem/walterphp">http://github.com/moisesduartem/walterphp</a> | $year - $fullName</small>
+    </div>
+
+</body>
+</html>
