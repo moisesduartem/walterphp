7631f927a4676a7a627294ec745e6d80105ca868
Moises Mariano <moisesduartem@gmail.com>
2020-08-22 14:32:45 -0300

    feat: exibe tanto {{}} como $ (sintaxe)

--- views/inheritance/index.php
+++ views/inheritance/index.php
@@ -1,7 +1,7 @@
 {% extends 'parent.php' %}
 
 <div id="container">
-        <h3>Filme: $movie</h3>
+        <h3>Filme: {{ movie }}</h3>
         <h3>Nome: $name</h3>
         <a id="voltar" href="/">Voltar</a>
         <small><a href="http://github.com/moisesduartem/walterphp">http://github.com/moisesduartem/walterphp</a> | $year - $fullName</small>
